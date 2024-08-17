323500a365a7c90f7b834d2f3fc89849e9a81aa5
Matheus Henrique <69221000+matheushenriquemoreno@users.noreply.github.com>
2024-08-17 20:19:06 -0300

    Aplicando o conceito de Deployment, Services e setando variaveis de ambiente direto no Kubernets

--- Server/server.go
+++ Server/server.go
@@ -3,15 +3,19 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
 	fmt.Println("Servidor rodando na porta 80.")
 	http.HandleFunc("/", Hello)
-	http.ListenAndServe(":80", nil)
+	http.ListenAndServe(":8000", nil)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Requisição recebida!")
-	w.Write([]byte("<h1> ola Mundo FullCycle!!!!"))
+	name := os.Getenv("NAME")
+	age := os.Getenv("AGE")
+	fmt.Fprintf(w, "Ola, I'm %s. I'm %s", name, age)
+	//w.Write([]byte("<h1> ola Mundo FullCycle!!!!"))
 }
